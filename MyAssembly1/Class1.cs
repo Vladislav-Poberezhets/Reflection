cb7fe8e87f83ab942fa87a1b3c86db4a5e7ee9c8
Vladislav-Poberezhets <vladpoberezhets1@gmail.com>
2022-11-29 19:52:26 +0200

    Add project files.

---
+++ MyAssembly1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyAssembly
+{
+    public class MyClass
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public MyClass(int X, int Y)
+        {
+            this.X = X;
+            this.Y = Y;
+        }
+
+        public int MyMethod(int count, string text)
+        {
+            Console.WriteLine("This is a normal method.");
+            Console.WriteLine("Count: {0}", count);
+            Console.WriteLine("Text: {0}", text);
+
+            return X + Y;
+        }
+
+        public static void StaticMethod(int count, float radius)
+        {
+            Console.WriteLine("This is a static method call.");
+            Console.WriteLine("Count: {0}", count);
+            Console.WriteLine("Radius: {0}", radius);
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly asm = Assembly.LoadFrom(@"D:\C#\Reflection\MyAssembly1\bin\Debug\MyAssembly1.dll");
+
+            Type t = asm.GetType("MyAssembly.MyClass");
+
+            Console.WriteLine("Properties:");
+            foreach (PropertyInfo prop in t.GetProperties())
+            {
+                Console.WriteLine($"{prop.PropertyType} {prop.Name} ");
+            }
+
+            Console.WriteLine(new String('*', 10));
+
+
+            Console.WriteLine("Metods:");
+             foreach (MethodInfo member in t.GetMethods()
+            .Where(m => !m.Name.StartsWith("get_") && !m.Name.StartsWith("set_"))) 
+                {
+                Console.Write($"{member.ReturnType.Name} {member.Name} ");
+
+                ParameterInfo[] parameters = member.GetParameters();
+                for (int i = 0; i < parameters.Length; i++)
+                {
+                    var param = parameters[i];
+                    string modificator = "";
+                    if (param.IsIn) modificator = "in";
+                    else if (param.IsOut) modificator = "out";
+
+                    Console.Write($" {param.ParameterType.Name} {modificator} {param.Name}");
+                    
+                }
+                Console.WriteLine(" |||||");
+            }
+        }
+
+
+    }
+}
